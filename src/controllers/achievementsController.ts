a7a3be41e442a901133d4dd3a2e95fed9de13de6
martynassimanavicius <martynas.simanavicius2@gmail.com>
2024-11-15 11:18:01 +0200

    update image quick fix

--- src/controllers/achievementsController.ts
+++ src/controllers/achievementsController.ts
@@ -1,3 +1,14 @@
 import pool from '../config/db';
 import { RowDataPacket } from 'mysql2';
 
+export const createAchievement = async (name: string, description: string, conditionQuery: string): Promise<boolean> => {
+    try {
+        const query = 'INSERT INTO achievements (name, description, condition_query) VALUES (?, ?, ?)';
+        await pool.query(query, [name, description, conditionQuery]);
+    
+        return true;
+    } catch (error: any | Error) {
+        throw new Error(`Error creating achievement: ${error.message}`);
+    }
+}
+
--- src/controllers/imagesController.ts
+++ src/controllers/imagesController.ts
@@ -41,12 +41,9 @@ export const getImageByUserId = async (userId: string): Promise<string[]> => {
 export const updateImageByUserId = async (file: Express.Multer.File, userId: string): Promise<string> => {
     try {
         const queryGetImageUrl = 'SELECT url FROM images WHERE user_id = ?';
-        const queryDeleteImage = 'DELETE FROM images WHERE user_id = ?';
+        const queryUpdateImage = 'UPDATE images SET url = ? WHERE user_id = ?';
 
         const [rows] = await pool.query<RowDataPacket[]>(queryGetImageUrl, [userId]);
-        console.log(rows);
-
-        await pool.query(queryDeleteImage, [userId]);
 
         const fileName = (rows[0] as RowDataPacket).url;
         const blob = bucket.file(fileName as string);
@@ -58,6 +55,8 @@ export const updateImageByUserId = async (file: Express.Multer.File, userId: str
         });
         await blob.makePublic();
         const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
+
+        await pool.query(queryUpdateImage, [fileName, userId]);
         return publicUrl;
     } catch (err: any | Error) {
         throw new Error('failed to update the image');
