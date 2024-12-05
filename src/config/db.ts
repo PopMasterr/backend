c76c126563e9aa9de36689a89193be7aa88f7fac
martynassimanavicius <martynas.simanavicius2@gmail.com>
2024-12-05 22:05:42 +0200

    new database
    1) setting up new database

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,13 +3,20 @@ import 'dotenv/config';
 import * as url from 'url';
 import { create } from 'domain';
 
-const dbUrl = process.env.JAWSDB_URL || '';
+// const dbUrl = process.env.JAWSDB_URL || '';
 
-const connectionParams = url.parse(dbUrl);
-const [user, password] = (connectionParams.auth || '').split(':');
-const [host, port] = (connectionParams.host || '').split(':');
-const database = (connectionParams.pathname || '').substring(1);
-const port2 = parseInt(port || '3306');
+// const connectionParams = url.parse(dbUrl);
+// const [user, password] = (connectionParams.auth || '').split(':');
+// const [host, port] = (connectionParams.host || '').split(':');
+// const database = (connectionParams.pathname || '').substring(1);
+// const port2 = parseInt(port || '3306');
+
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+const host = process.env.DB_HOST;
+const database = process.env.DB_NAME;
+const port2 = Number(process.env.DB_PORT);
+
 
 const pool = mysql.createPool({
     host: host,
@@ -18,7 +25,7 @@ const pool = mysql.createPool({
     database: database,
     port: port2,
     waitForConnections: true,
-    connectionLimit: 10,
+    connectionLimit: 100,
     connectTimeout: 10000,
     queueLimit: 0,
 });
